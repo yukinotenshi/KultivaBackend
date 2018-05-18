5199179c91eb5e6925861e20e80430bb6ace2c9d
yukinotenshi <gabrielbentararaphael@gmail.com>
2018-05-18 18:46:10 +0700

    added all products endpoint

--- controller/product_controller.py
+++ controller/product_controller.py
@@ -1,5 +1,6 @@
 from controller.base_controller import Controller
 from model.product_model import *
+from model.base_model import Product
 
 class ProductController(Controller):
     def all_category(self):
@@ -10,3 +11,19 @@ class ProductController(Controller):
 
     def get_product_detail(self):
         return get_product_detail(self.data[0])
+
+    def all_product(self):
+        products = Product.select()
+        data = {
+            "products" : []
+        }
+
+        for p in products:
+            data["products"].append({
+                "id" : p.id,
+                "name" : p.name,
+                "price" : p.harga,
+                "image" : p.image
+            })
+
+        return data
